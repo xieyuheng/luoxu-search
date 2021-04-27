abcca4470f4579e960e044b2993c1372639227fb
Xie Yuheng <xyheme@gmail.com>
2021-04-27 19:51:11 +0800

    whitespace-pre-line

--- src/components/message-card/message-card.vue
+++ src/components/message-card/message-card.vue
@@ -3,7 +3,7 @@
     <p class="flex justify-between py-1 text-xs font-bold text-gray-600">
       {{ message.from_name }}
     </p>
-    <p class="py-2 break-words">{{ message.text }}</p>
+    <p class="py-2 break-words whitespace-pre-line">{{ message.text }}</p>
     <div class="flex justify-end">
       <a :href="message.tg_link" class="underline">
         <p class="hover:text-gray-900 text-sm text-gray-500">
