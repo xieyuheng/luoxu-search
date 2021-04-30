d738b9bfa3ceca380a81c5d4ab5d029d4de7feb3
Eric Little <reallyimeric@gmail.com>
2021-04-30 22:49:18 +0800

    fix: add query into key

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -33,7 +33,11 @@
         <div class="px-1 py-2 text-gray-500">
           找到 {{ state.messages.length }} 条消息 --
         </div>
-        <div v-for="message in state.messages" class="py-2" :key="message.id">
+        <div
+          v-for="message in state.messages"
+          class="py-2"
+          :key="message.id + ',' + state.query"
+        >
           <message-card :message="message" :search="state.query" />
         </div>
       </div>
